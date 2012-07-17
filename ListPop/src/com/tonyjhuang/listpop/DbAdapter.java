3ae7451d0d72e719c5b622013d3230ad70952220
tony huang <tony.huang.jun@gmail.com>
2012-07-17 14:59:15 -0400

    Trimmed StartActivity, working on new method of storing arraylists

--- ListPop/src/com/tonyjhuang/listpop/DbAdapter.java
+++ ListPop/src/com/tonyjhuang/listpop/DbAdapter.java
@@ -1,11 +1,10 @@
 package com.tonyjhuang.listpop;
 
-
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 
 import android.content.ContentValues;
@@ -15,7 +14,11 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Base64;
 import android.util.Log;
+
+
+
 
 public class DbAdapter {
 
@@ -51,22 +54,50 @@ public class DbAdapter {
 		mListPopDBOpenHelper.close();
 	}
 	
-	//Insert ArrayList as byte array
-	public long enterList(String list_header, ArrayList<String> array) throws IOException, SQLException {
-		ByteArrayOutputStream bout = new ByteArrayOutputStream();
-		DataOutputStream dout = new DataOutputStream(bout);
-		for(String s : array){
-			dout.writeChars(s);
-		}
-		dout.close();
-		byte[] arrayList = bout.toByteArray();
-		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public long enterList(String list_header, ArrayList<String> array) throws FileNotFoundException, IOException{
 		ContentValues values = new ContentValues();
 		values.put(KEY_LIST_HEADER_COLUMN, list_header);
-		values.put(KEY_LIST_COLUMN, arrayList);	
+		values.put(KEY_LIST_COLUMN, toString(array));	
 		
 		return mDb.insert(DATABASE_TABLE, null, values);
 	}
+	
+	
+	
+	 //Write the object to a Base64 string. 
+    private static String toString( Serializable o ) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream( baos );
+        oos.writeObject( o );
+        oos.close();
+        return new String( Base64.encode( baos.toByteArray(), 0 ) );
+    }
+    
+  
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	public boolean deleteById(long id){
 		return mDb.delete(DATABASE_TABLE,  KEY_ROWID+"="+id, null) > 0;
@@ -84,30 +115,6 @@ public class DbAdapter {
 			mCursor.moveToFirst();
 		}
 		return mCursor;
-	}
-	
-	public ArrayList<String> fetchArrayListByRowId(long rowId) throws IOException, SQLException {
-		Cursor mCursor =
-				mDb.query(true, DATABASE_TABLE, new String[] {KEY_LIST_COLUMN},
-						KEY_ROWID+"="+rowId, null, null, null, null, null);
-		if (mCursor != null){
-			mCursor.moveToFirst();
-		
-			int KEY_LIST_COLUMN_INDEX = mCursor.getColumnIndexOrThrow(KEY_LIST_COLUMN);
-			ArrayList<String> strings = new ArrayList<String>();
-		
-			byte[] asBytes = mCursor.getBlob(KEY_LIST_COLUMN_INDEX);
-			
-			ByteArrayInputStream bin = new ByteArrayInputStream(asBytes);
-	        DataInputStream din = new DataInputStream(bin);
-	        
-	        while (din.available() > 0) {
-	            strings.add(din.readUTF());
-	        }
-	        din.close();
-	        return strings;
-		}
-		return null;
 	}
 	
 	//Returns a cursor with all columns' row id's and byte arrays.
--- ListPop/src/com/tonyjhuang/listpop/StartActivity.java
+++ ListPop/src/com/tonyjhuang/listpop/StartActivity.java
@@ -3,9 +3,9 @@ package com.tonyjhuang.listpop;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import android.annotation.SuppressLint;
+
 import android.app.Activity;
-import android.content.Intent;
+//import android.content.Intent;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.os.Bundle;
@@ -13,16 +13,13 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class StartActivity extends Activity {
 	
 	Button chooseButton;
 	private DbAdapter mDbA;
-	private TextView debugCount;
 	private Cursor cIndex;
 	Button nextButton;
-	private TextView debugTitle;
 	Button moveToPop;
 	
 	
@@ -52,69 +49,51 @@ public class StartActivity extends Activity {
         
     }
 
+    
+    
     //Attaches onClickListener to chooseButton
     private void hookUpChoose(){
     	chooseButton.setOnClickListener(new OnClickListener(){
         	public void onClick(View v){
         		setContentView(R.layout.choose);
         		
-        		debugCount = (TextView)findViewById(R.id.debugcounttextview);
-                debugCount.setText(Long.toString(mDbA.fetchNumberOfLists()));
-                
-                debugTitle = (TextView)findViewById(R.id.debugtitletextview);
-                
+        		
                 nextButton = (Button)findViewById(R.id.nextbutton);
                 hookUpNext();
                 
                 moveToPop = (Button)findViewById(R.id.popbutton);
                 hookUpPop();
+                
         	}
         });
     }
+    
+    
     
     private void hookUpNext(){
     	nextButton.setOnClickListener(new OnClickListener(){
 			@Override
 			public void onClick(View v) {
 				if((cIndex == null) || (cIndex.isAfterLast())){
-					debugDB();
+					cIndex = mDbA.fetchAll();
 				}
-				debugNewText();
 			}
     	});
     }
     
-    @SuppressLint("ParserError")
+    
+    
 	private void hookUpPop(){
     	moveToPop.setOnClickListener(new OnClickListener(){
     		@Override
     		public void onClick(View v) {
-    			int KEY_ROWID_COLUMN_INDEX = cIndex.getColumnIndexOrThrow(
-    					DbAdapter.KEY_ROWID);
-    			int rowid = cIndex.getInt(KEY_ROWID_COLUMN_INDEX);
-    			long _rowid = Long.valueOf(rowid);
-    			
-    			
-    			ArrayList<String> currentArray = 
-    					fetchArrayListByRowId(_rowid);
+    			/*
     			Intent goToPop = new Intent(StartActivity.this, PopActivity.class);
     			goToPop.putStringArrayListExtra("arraylist", currentArray);
     			StartActivity.this.startActivity(goToPop);
+    			*/
     		}
     	});
-    }
-    
-    private ArrayList<String> fetchArrayListByRowId(long rowid){
-    	try {
-			return mDbA.fetchArrayListByRowId(rowid);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	return null;
     }
     
     private void debugInitialize() throws SQLException, IOException{
@@ -139,18 +118,6 @@ public class StartActivity extends Activity {
     	mDbA.enterList("list 1", array1);
     	mDbA.enterList("list 2", array2);
     	mDbA.enterList("LIST THREE", array3);
-    }
-    
-    private void debugNewText(){
-    	String title = 
-				cIndex.getString(cIndex.getColumnIndexOrThrow(
-						DbAdapter.KEY_LIST_HEADER_COLUMN));
-		debugTitle.setText(title);
-		cIndex.moveToNext();
-    }
-    
-    private void debugDB(){
-    	cIndex = mDbA.fetchAll();
     }
     
     @Override
