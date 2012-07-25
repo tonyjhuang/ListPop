46882bc1a8db322cc79fec90867608c9f17cf2aa
tony huang <tony.huang.jun@gmail.com>
2012-07-25 17:53:59 -0400

    Last commit before signing off. Fixed some indent issues. Moved hardcoded dimensions in xml files into dimens.xml.

--- ListPop/gen/com/tonyjhuang/listpop/R.java
+++ ListPop/gen/com/tonyjhuang/listpop/R.java
@@ -19,6 +19,7 @@ public final class R {
         public static final int padding_medium=0x7f040001;
         public static final int padding_small=0x7f040000;
         public static final int side_margin=0x7f040003;
+        public static final int title_indent=0x7f040005;
         public static final int to_text=0x7f040004;
     }
     public static final class drawable {
@@ -66,31 +67,30 @@ public final class R {
         public static final int start=0x7f080000;
     }
     public static final class string {
-        public static final int add=0x7f050008;
-        public static final int addlistitem=0x7f05000c;
-        public static final int addlistitemprompt=0x7f05000d;
-        public static final int addlistname=0x7f05000a;
-        public static final int addlistnameprompt=0x7f05000b;
+        public static final int add=0x7f050007;
+        public static final int addlistitem=0x7f05000b;
+        public static final int addlistitemprompt=0x7f05000c;
+        public static final int addlistname=0x7f050009;
+        public static final int addlistnameprompt=0x7f05000a;
         public static final int app_name=0x7f050000;
-        public static final int choose=0x7f050003;
-        public static final int chooseButton=0x7f050004;
-        public static final int edit=0x7f050009;
-        public static final int finishednewlist=0x7f05000e;
-        public static final int menu_delete=0x7f05000f;
+        public static final int choose=0x7f050002;
+        public static final int chooseButton=0x7f050003;
+        public static final int edit=0x7f050008;
+        public static final int finishednewlist=0x7f05000d;
+        public static final int menu_delete=0x7f05000e;
         public static final int menu_settings=0x7f050001;
-        public static final int nexttext=0x7f050005;
-        public static final int no_header=0x7f050011;
-        public static final int no_items=0x7f050010;
-        public static final int number_range_menu_title=0x7f050013;
-        public static final int optional=0x7f050016;
-        public static final int poplist=0x7f050007;
-        public static final int poptext=0x7f050006;
-        public static final int preset=0x7f050012;
-        public static final int preset_name_header=0x7f050015;
-        public static final int preset_spinner_prompt=0x7f050014;
-        public static final int title_activity_start=0x7f050002;
-        public static final int to=0x7f050018;
-        public static final int yes_no_fragment_text=0x7f050017;
+        public static final int nexttext=0x7f050004;
+        public static final int no_header=0x7f050010;
+        public static final int no_items=0x7f05000f;
+        public static final int number_range_menu_title=0x7f050012;
+        public static final int optional=0x7f050015;
+        public static final int poplist=0x7f050006;
+        public static final int poptext=0x7f050005;
+        public static final int preset=0x7f050011;
+        public static final int preset_name_header=0x7f050014;
+        public static final int preset_spinner_prompt=0x7f050013;
+        public static final int to=0x7f050017;
+        public static final int yes_no_fragment_text=0x7f050016;
     }
     public static final class style {
         public static final int AppTheme=0x7f070000;
