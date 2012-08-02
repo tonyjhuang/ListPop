c341d790f6a0adc92994fee12e11e8aa836924c6
tony huang <tony.huang.jun@gmail.com>
2012-08-02 17:10:22 -0400

    Commented out TAG and Log in AddActivity.

--- ListPop/src/com/tonyjhuang/listpop/AddActivity.java
+++ ListPop/src/com/tonyjhuang/listpop/AddActivity.java
@@ -4,7 +4,7 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+//import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,7 +19,7 @@ public class AddActivity extends Activity {
 	ListView currentEntries;
 	Button finish;
 	AddArrayAdapter aa;
-	private static final String TAG = "AddActivity";
+	//private static final String TAG = "AddActivity";
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
