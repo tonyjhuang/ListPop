e1e1c2b74c45c773fa7242b7c08e5db82e355ffc
tony huang <tony.huang.jun@gmail.com>
2012-08-18 17:04:41 -0400

    Replaced list_bg with 9patch image in hdpi. Added list_padding dimension. Tweaked layout_normal start.xml.

--- ListPop/gen/com/tonyjhuang/listpop/R.java
+++ ListPop/gen/com/tonyjhuang/listpop/R.java
@@ -25,6 +25,7 @@ public final class R {
         public static final int bg=0x7f080000;
     }
     public static final class dimen {
+        public static final int list_padding=0x7f050006;
         public static final int padding_large=0x7f050002;
         public static final int padding_medium=0x7f050001;
         public static final int padding_small=0x7f050000;
@@ -39,23 +40,23 @@ public final class R {
         public static final int edit=0x7f020003;
         public static final int icon=0x7f020004;
         public static final int list_bg=0x7f020005;
-        public static final int listbg=0x7f020006;
-        public static final int splash=0x7f020007;
-        public static final int trash=0x7f020008;
+        public static final int splash=0x7f020006;
+        public static final int trash=0x7f020007;
     }
     public static final class id {
-        public static final int add_menu=0x7f0b0022;
+        public static final int add_menu=0x7f0b0023;
         public static final int additem=0x7f0b0007;
-        public static final int custom=0x7f0b0023;
+        public static final int banner=0x7f0b001d;
+        public static final int custom=0x7f0b0024;
         public static final int deleteline=0x7f0b0011;
         public static final int edititem=0x7f0b000b;
         public static final int editline=0x7f0b0013;
         public static final int eightballlist=0x7f0b000c;
         public static final int finish=0x7f0b0009;
         public static final int finishbutton=0x7f0b0005;
-        public static final int fragment_place=0x7f0b001e;
+        public static final int fragment_place=0x7f0b001f;
         public static final int fragmentframe=0x7f0b001a;
-        public static final int imageView1=0x7f0b001d;
+        public static final int imageView1=0x7f0b001e;
         public static final int lineItem=0x7f0b0012;
         public static final int list=0x7f0b0008;
         public static final int listitem=0x7f0b0010;
@@ -66,22 +67,22 @@ public final class R {
         public static final int listnameheader=0x7f0b0000;
         public static final int listselection=0x7f0b001c;
         public static final int lowerindex=0x7f0b000d;
-        public static final int menu_add=0x7f0b0021;
-        public static final int menu_edit=0x7f0b0020;
+        public static final int menu_add=0x7f0b0022;
+        public static final int menu_edit=0x7f0b0021;
         public static final int newitem=0x7f0b000a;
         public static final int newitems=0x7f0b0004;
         public static final int pop=0x7f0b0016;
         public static final int popresult=0x7f0b0015;
-        public static final int preset=0x7f0b0024;
+        public static final int preset=0x7f0b0025;
         public static final int presetnameheader=0x7f0b0017;
         public static final int presetspinner=0x7f0b0018;
         public static final int presettitle=0x7f0b0019;
         public static final int relative=0x7f0b001b;
         public static final int rlayout=0x7f0b0014;
-        public static final int tab=0x7f0b0025;
+        public static final int tab=0x7f0b0026;
         public static final int totext=0x7f0b000f;
         public static final int upperindex=0x7f0b000e;
-        public static final int yes_no_text=0x7f0b001f;
+        public static final int yes_no_text=0x7f0b0020;
     }
     public static final class layout {
         public static final int add=0x7f030000;
