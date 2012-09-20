80899b3c6945c006676142d2e42785b477d5ddbb
boxfactory <tony.huang.jun@gmail.com>
2012-09-20 00:33:10 -0400

    Moved layouts to multipurpose layout folder. It's not like I'm making layouts for each screen size, right?

--- ListPop/gen/com/tonyjhuang/listpop/R.java
+++ ListPop/gen/com/tonyjhuang/listpop/R.java
@@ -917,8 +917,8 @@ containing a value of this type.
         public static final int abs__titleDivider=0x7f05001c;
         public static final int abs__up=0x7f05000b;
         public static final int additem=0x7f050028;
-        public static final int banner=0x7f050047;
-        public static final int bottle=0x7f05003b;
+        public static final int banner=0x7f050042;
+        public static final int bottle=0x7f050039;
         public static final int bubblelarge=0x7f05003c;
         public static final int bubblesmall=0x7f05003d;
         public static final int deleteline=0x7f050035;
@@ -927,10 +927,8 @@ containing a value of this type.
         public static final int editline=0x7f050037;
         public static final int eightballlist=0x7f050030;
         public static final int finish=0x7f05002a;
-        public static final int finishbutton=0x7f050042;
-        public static final int fragment_place=0x7f050048;
-        public static final int fragmentframe=0x7f050041;
-        public static final int header=0x7f050043;
+        public static final int fragmentframe=0x7f050040;
+        public static final int header=0x7f05003e;
         public static final int homeAsUp=0x7f050006;
         public static final int imageView1=0x7f05002d;
         public static final int lineItem=0x7f050036;
@@ -938,25 +936,22 @@ containing a value of this type.
         public static final int listMode=0x7f050002;
         public static final int listitem=0x7f050034;
         public static final int listname=0x7f050027;
-        public static final int listselection=0x7f050046;
+        public static final int listselection=0x7f050043;
         public static final int lowerindex=0x7f050031;
-        public static final int menu_add=0x7f05004a;
-        public static final int menu_edit=0x7f050049;
+        public static final int menu_add=0x7f050045;
+        public static final int menu_edit=0x7f050044;
         public static final int newitem=0x7f05002e;
         public static final int normal=0x7f050001;
         public static final int num_picker=0x7f05002c;
         public static final int pop=0x7f05003a;
-        public static final int popresult=0x7f050039;
+        public static final int popresult=0x7f05003b;
         public static final int preset=0x7f05002b;
-        public static final int presetnameheader=0x7f05003e;
-        public static final int presetspinner=0x7f05003f;
-        public static final int presettitle=0x7f050040;
-        public static final int relative=0x7f050045;
+        public static final int relative=0x7f050041;
         public static final int rlayout=0x7f050038;
         public static final int showCustom=0x7f050008;
         public static final int showHome=0x7f050005;
         public static final int showTitle=0x7f050007;
-        public static final int spinner=0x7f050044;
+        public static final int spinner=0x7f05003f;
         public static final int tabMode=0x7f050003;
         public static final int totext=0x7f050033;
         public static final int upperindex=0x7f050032;
@@ -1004,7 +999,6 @@ containing a value of this type.
         public static final int sherlock_spinner_dropdown_item=0x7f030022;
         public static final int sherlock_spinner_item=0x7f030023;
         public static final int start=0x7f030024;
-        public static final int tabs=0x7f030025;
     }
     public static final class menu {
         public static final int action_menu=0x7f0d0000;
